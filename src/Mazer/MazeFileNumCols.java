071676b1e5c066c5e9cb7c86cc67ecab31865fa7
David Pinto <daxtery11@gmail.com>
2017-06-09 12:59:14 +0100

    better

--- src/Mazer/MazeFileNumCols.java
+++ src/Mazer/MazeFileNumCols.java
@@ -5,4 +5,15 @@ package Mazer;
  */
 public class MazeFileNumCols extends MazeException {
 
+    private int coln;
+    private String message = "Todas as linhas têm de ter o mesmo número de caracters. Erro encontrado na linha nº:" + coln;
+
+    public String getMessage(){
+        return message;
+    }
+
+    public MazeFileNumCols(int col){
+        this.coln = col;
+    }
+
 }
--- src/Mazer/MazeFileWrongChar.java
+++ src/Mazer/MazeFileWrongChar.java
@@ -4,4 +4,17 @@ package Mazer;
  * Created by PC on 09/06/2017.
  */
 public class MazeFileWrongChar extends MazeException {
+
+    private int rown;
+    private int coln;
+    private String message = "Caráter inválido encontrado na posição: " + rown + coln;
+
+    public String getMessage(){
+        return message;
+    }
+
+    public MazeFileWrongChar(int row, int col){
+        this.rown = row;
+        this.coln = col;
+    }
 }
