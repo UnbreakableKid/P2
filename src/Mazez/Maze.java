384c9f058518971e99ae5c3e30f3a3c5a3fe5933
daxtery <daxtery11@gmail.com>
2017-06-16 00:26:47 +0100

    !

--- src/Mazez/Maze.java
+++ src/Mazez/Maze.java
@@ -1,183 +1,396 @@
 package Mazez;
 
+import java.awt.Point;
 import java.io.*;
-import javax.swing.*;
-import javax.swing.filechooser.FileNameExtensionFilter;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
 
 public class Maze implements IMaze {
 
+    private int mode = 0;
+
+    private int i = 0;
+    private Random r = new Random();;
+
     private final int ERROR = -1;
-    private File f = new File("E:/PC/New/P2/src/d.maze");
+    private byte[] stuff;
+
     private MazeCell[][] maze;
-    private int numCols;
-    private int numRows;
-    private Pawn p;
-    private Route r;
+    private int numColS, numRowS, numCols, numRows, numColE, numRowE;
+
+    private ArrayList<Point> casasAntigas;
+
+    private static final int[] north = new int[]{-1, 0};
+    private static final int[] south = new int[]{1, 0};
+    private static final int[] west = new int[]{0, -1};
+    private static final int[] east = new int[]{0, 1};
+
+    private static final int VERTICAL = 1;
+    private static final int HORIZONTAL = 2;
+
+    public Maze(int rows, int cols){
+        numRows = rows;
+        numCols = cols;
+        maze =  new MazeCell[numRows][numCols];
+        mode = 1;
+        try{
+            generate();
+        }catch (Exception e){
+            e.getMessage();
+        }
+        mode = 0;
+    }
+
+    public Maze(File f) throws Exception {
+
+        if (goodCols(f)) {
+            if (goodtoRead(f)) {
+                read();
+            }
+        }
+    }
+
+    void generate() {
+
+        setStartAndEnd();
+
+        int i = 0;
+
+        boolean canDo;
+
+        try {
+
+            do {
+                randomCells();
+                i++;
+
+                //if it gets stuckerino
+
+                if (i == 5) {
+                    System.out.print(1);
+                    throw new Exception("Couldn't do it");
+                }
+
+                canDo = Solve();
+
+            } while (!canDo);
+        } catch (Exception e) {
+            generate();
+        }
+    }
+
+    private MazeCell choose(int type){
+        switch (type){
+            case 0:
+                return MazeCell.EMPTY;
+            case 1:
+                return MazeCell.WALL;
+            case 2:
+                return MazeCell.EMPTY;
+        }
+        return MazeCell.EMPTY;
+    }
+
+    private void randomCells(){
+
+        for (int i = 0; i < numRows; i++){
+            for (int ii = 0; ii < numCols; ii++){
+                if ((i == numRowS && ii == numColS) || (i == numRowE && ii == numColE)){
+                    continue;
+                }
+                MazeCell type = choose(r.nextInt(2));
+                maze[i][ii] = type;
+            }
+        }
+    }
+
+    void printMaze() {
+        int a = 0;
+        for (int i = 0; i < numRows; i++) {
+            for (int ii = 0; ii < numCols; ii++) {
+                a++;
+                System.out.println(maze[i][ii].toString());
+            }
+        }
+        System.out.println(a);
+    }
+
+    private void setStartAndEnd(){
+
+        numRowS = 0;
+        numColS = r.nextInt(numCols);
+
+        maze[numRowS][numColS] = MazeCell.START;
+
+        do {
+
+            numColE = r.nextInt(numCols);
+
+        }while (numColS == numColE);
+
+        numRowE = r.nextInt(numRows);
+
+        maze[numRowE][numColE] = MazeCell.EXIT;
+
+    }
+
+
+    boolean Solve() {
+
+        casasAntigas = new ArrayList<Point>();
+        pawns = new LinkedList<Pawn>();
+        Pawn p = new Pawn(this);
+        pawns.add(p);
+        Route rou = new Route();
+
+        try {
+
+            //while there are pawns to solve
+
+            while (!isSolvedBy(pawns.peek())) {
+
+                //System.out.println(pawns.peek());
+                Point p = new Point(pawns.peek().position()[0], pawns.peek().position()[1]);
+                if (!casasAntigas.contains(p)) {
+                    //System.out.println("Marked a cell");
+                    casasAntigas.add(p);
+                }
+
+                Move[] moves = getOptions(pawns.peek());
+
+                if (moves.length == 0) {
+
+                    // This path didn't work
+                    // tirar o ultimo.
+                    pawns.poll();
+                    //se ja não houver excecção e depois false
+                } else {
+
+                    if (moves.length > 1) {
+                        // more than 1 way to go
+                        //System.out.println("Ramificação");
+
+                        for (int i = 1; i < moves.length; i++) {
+
+                            Pawn alt = new Pawn(this);
+                            alt.setPosition(pawns.peek().position());
+                            alt.move(moves[i]);
+                            rou.move(moves[i]);
+                            pawns.add(alt);
+                            //System.out.println(pawns);
+                        }
+                    }
+                    //keeps indo para onde ia
+                    pawns.peek().move(moves[0]);
+                    rou.move(moves[moves[0]]);
+                }
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
 
     public Move[] getOptions(Pawn p) {
-        Move[] movez = new Move[1];
-        return movez;
+        ArrayList<Move> move = new ArrayList<Move>();
+        for (Move m : Move.values()) {
+            if (canMove(p, m)) {
+                //System.out.println("Can Move "+m);
+                move.add(m);
+            }
+        }
+        return move.toArray(new Move[move.size()]);
+    }
+
+    static int[] identifyM(Move m) {
+        switch (m) {
+            case NORTH:
+                return north;
+            case SOUTH:
+                return south;
+            case WEST:
+                return west;
+            case EAST:
+                return east;
+            case NOOP:
+        }
+        return new int[]{0, 0};
+    }
+
+    static int[] addMatrizes(int[] a, int[] b) {
+        int c[] = new int[a.length];
+
+        for (int i = 0; i < a.length; i++) {
+            c[i] = a[i] + b[i];
+        }
+        return c;
+    }
+
+    int[] getEndP() {
+        return new int[]{numRowE, numColE};
+    }
+
+    int[] getStartP() {
+        return new int[]{numRowS, numColS};
     }
 
     public boolean isSolvedBy(Pawn p) {
-        return true;
+        int[] a = p.position();
+        return a[0] == numRowE & a[1] == numColE;
     }
 
     public void move(Pawn p, Move m) {
-
+        p.move(m);
     }
 
     public boolean canMove(Pawn p, Move m) {
+        int[] pos = p.position();
+        int[] posToBe = addMatrizes(identifyM(m), pos);
+        if (mode == 1){
+            if(casasAntigas.contains(new Point(posToBe[0], posToBe[1]))){
+                return false;
+            }
+        }
+        try {
+            return (canIgo(maze[posToBe[0]][posToBe[1]]));
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return false;
+        }
+    }
+
+    private boolean canIgo(MazeCell mc) {
+        switch (mc) {
+            case EXIT:
+                return true;
+            case WALL:
+                return false;
+            case EMPTY:
+                return true;
+            case START:
+                return true;
+        }
+        return false;
+    }
+
+
+    /*for opening and doing maze
+
+
+     */
+
+
+    boolean goodCols(File f) throws Exception {
+
+        FileInputStream fis = new FileInputStream(f);
+        InputStreamReader isr = new InputStreamReader(fis);
+        BufferedReader reader = new BufferedReader(isr);
+        String firstLine = reader.readLine();
+        numCols = firstLine.length();
+        numRows = 0;
+        int i = 1;
+        while (firstLine != null) {
+            numRows++;
+            if (firstLine.length() != numCols) {
+                numCols = ERROR;
+                numRows = ERROR;
+                throw new MazeFileNumCols(i);
+            }
+            firstLine = reader.readLine();
+            i++;
+        }
+        reader.close();
+        isr.close();
+        fis.close();
         return true;
     }
 
-    public Maze() {
+    boolean goodtoRead(File f) throws Exception {
 
-        this.p = new Pawn();
+        FileInputStream fis = new FileInputStream(f);
+        int filesize = (int) f.length();
+        this.stuff = new byte[filesize];
+        fis.read(stuff);
+        int a, cC = 0, cR = 0;
 
-    }
+        for (byte b : stuff) {
+            a = (int) b;
 
-    public void iWonder() {
-        for (MazeCell m : maze[1]) {
-            System.out.print(m);
-        }
-    }
-
-    private void chooseOne() {
-        JButton b = new JButton();
-        JFileChooser chooser = new JFileChooser();
-        FileNameExtensionFilter filter = new FileNameExtensionFilter("Maze file", "maze");
-        chooser.setFileFilter(filter);
-        int returnVal = chooser.showOpenDialog(b);
-        if (returnVal == JFileChooser.APPROVE_OPTION) {
-            System.out.println("You chose to open this file: " + chooser.getSelectedFile().getName());
-            f = chooser.getSelectedFile();
-        }
-    }
-
-
-    public void openFile() {
-
-        try {
-            //chooseOne();
-            FileInputStream fis = new FileInputStream(this.f);
-            InputStreamReader isr = new InputStreamReader(fis);
-            BufferedReader reader = new BufferedReader(isr);
-            String firstLine = reader.readLine();
-            numCols = firstLine.length();
-            numRows = 0;
-            int i = 1;
-            while (firstLine != null) {
-                numRows++;
-                if (firstLine.length() != numCols) {
-                    numCols = ERROR;
-                    numRows = ERROR;
-                    throw new MazeFileNumCols(i);
-                }
-                firstLine = reader.readLine();
-                i++;
+            if (!isValid(a)) {
+                invalidChar(cR, cC);
             }
-            maze = new MazeCell[numRows][numCols];
-            reader.close();
-            isr.close();
-            fis.close();
 
-            FileInputStream fis2 = new FileInputStream(this.f);
+            cC++;
 
-            int fsize = (int) f.length();
-            byte[] stuff = new byte[fsize];
-            fis2.read(stuff);
-            judge(stuff);
-
-
-        } catch (MazeFileWrongChar e) {
-            System.out.println(e.getMessage());
-        } catch (MazeFileNumCols e) {
-            System.out.println(e.getMessage());
-        } catch (Exception e) {
-            System.out.print(e.toString());
+            if (a == 10) {
+                cC = 0;
+                cR++;
+            }
         }
+        fis.close();
+        return true;
+    }
+
+    MazeCell[][] getMaze() {
+        return this.maze;
+    }
+
+    int[] getDims (){
+        return new int[]{numRows, numCols};
+    }
+
+    private void invalidChar(int cR, int cC) throws MazeFileWrongChar {
+        throw new MazeFileWrongChar(cR, cC);
     }
 
     private void addTo(MazeCell type, int cR, int cC) {
         this.maze[cR][cC] = type;
     }
 
-    private void judge(byte[] file) throws MazeFileWrongChar {
+    private void read() {
+        maze = new MazeCell[numRows][numCols];
+        //System.out.println(numRows);
+        //System.out.println(numCols);
         int cC = 0, cR = 0;
         int a;
-        for (byte b : file) {
+        for (byte b : this.stuff) {
             a = (int) b;
-            System.out.println(a);
-
-            if (isValid(a)){
-
-                if (a == 95){
+            switch (a) {
+                case 95:
                     addTo(MazeCell.EMPTY, cR, cC); // _
                     cC++;
-                }
-                if (a == 87){
+                    break;
+
+                case 87:
                     addTo(MazeCell.WALL, cR, cC); // W
                     cC++;
-                }
-                if (a == 83){
+                    break;
+
+                case 83:
                     addTo(MazeCell.START, cR, cC); // S
+                    numColS = cC;
+                    numRowS = cR;
                     cC++;
-                }
-                if (a == 69){
+                    break;
+
+                case 69:
                     addTo(MazeCell.EXIT, cR, cC); // E
+                    numColE = cC;
+                    numRowE = cR;
                     cC++;
-                }
-                if (a == 10){
-                    cC = 0;
+                    break;
+                case 10:
                     cR++;
-                }
-            }
-            if (!isValid(a)) {
-                throw new MazeFileWrongChar(cC, cR);
+                    cC = 0;
+                    break;
             }
         }
-     }
+    }
 
-    private boolean isValid(int ascii){
-        if (ascii == 95 | ascii == 83 | ascii == 87 | ascii == 13 | ascii == 10 | ascii == 69)
-            return true;
-        else
-            return false;
+    private boolean isValid(int ascii) {
+        return ascii == 95 | ascii == 83 | ascii == 87 | ascii == 13 | ascii == 10 | ascii == 69;
     }
 }
 
-class Pawn implements IPawn {
-    public void move(Move m) {
-    }
-
-    public Route getRoute() {return new Route();}
-}
-
-class Route implements IRoute {
-
-    private Move[] route;
-
-    public int getCol() {
-        return 1;
-    }
-
-    public int getRow() {
-        return 2;
-    }
-
-    public int getCol(int i) {
-        return 3;
-    }
-
-    public int getRow(int i) {
-        return 4;
-    }
-
-    public int length() {
-        return 5;
-    }
-
-    public void move(Move m) {
-    }
-}
